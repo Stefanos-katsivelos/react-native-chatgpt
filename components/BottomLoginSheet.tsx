57338f7d9aed79e6056177b872e10413e2cd4c56
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2025-03-08 22:14:20 +0200

    Initial commit

---
+++ components/BottomLoginSheet.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const BottomLoginSheet = () => {
+  return (
+    <View>
+      <Text>BottomLoginSheet</Text>
+    </View>
+  );
+};
+
+export default BottomLoginSheet;
